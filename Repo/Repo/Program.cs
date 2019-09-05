48751ac1f02479410ee935053e8b5f149acf053b
Clarence Wong <cwong54@nait.ca>
2019-09-05 14:50:39 -0600

    Create solution re#2

--- Repo/Repo/Program.cs
+++ Repo/Repo/Program.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http; // for the HTTPClient Class
+using static System.Console; //for using the static methods of the Console class without having to reference the class name.
 
 namespace Repo
 {
+   
     class Program
     {
+        const string API_BASE = "http://www.gitignore.io/api/";
+        const string API_LIST_LINES = API_BASE + "list? format = lines";
         static void Main(string[] args)
         {
         }
